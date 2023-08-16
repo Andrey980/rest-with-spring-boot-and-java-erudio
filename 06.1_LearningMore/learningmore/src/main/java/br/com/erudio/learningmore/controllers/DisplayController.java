9eea4fa69ae3b9786e4ad651dc52883a3b369940
Andrey980 <andreymcunhasilva@gmail.com>
2023-08-16 00:15:10 -0300

    Update

--- 06.1_LearningMore/learningmore/src/main/java/br/com/erudio/learningmore/controllers/DisplayController.java
+++ 06.1_LearningMore/learningmore/src/main/java/br/com/erudio/learningmore/controllers/DisplayController.java
@@ -1,5 +1,13 @@
 package br.com.erudio.learningmore.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DisplayController {
 
+  @RequestMapping("api/display")
+  public String display() {
+    return "Olá";
+  }
 }
